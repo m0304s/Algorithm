05f0f07b708b09f54bb16489e45c53cb0b1e791f
MinSeok_Choi <120546936+m0304s@users.noreply.github.com>
2024-03-05 17:30:05 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String id;
+        Scanner kb = new Scanner(System.in);
+        id=kb.next();
+        kb.close();
+        System.out.println(id+"??!");
+    }
+}
