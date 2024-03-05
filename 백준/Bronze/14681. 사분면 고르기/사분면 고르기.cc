9a13db93a79bb69e6018e7d0a733c07ba111ca07
MinSeok_Choi <120546936+m0304s@users.noreply.github.com>
2024-03-05 17:46:23 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	int x, y;
+	scanf("%d", &x);
+	scanf("%d", &y);
+	if (x > 0 && y > 0)
+		printf("1\n");
+	else if (x > 0 && y < 0)
+		printf("4\n");
+	else if (x < 0 && y>0)
+		printf("2\n");
+	else
+		printf("3\n");
+	return 0;
+}
