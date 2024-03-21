22a60ebab49020ce65abe9b825991d2a64426dd4
MinSeok_Choi <120546936+m0304s@users.noreply.github.com>
2024-03-21 14:21:36 +0900

    [Silver I] Title: 곱셈, Time: 128 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -1,33 +1,27 @@
 import java.io.*;
-import java.util.Scanner;
-
 public class Main {
-    
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // 입력 받기
-        long A = scanner.nextLong();
-        long B = scanner.nextLong();
-        long C = scanner.nextLong();
-
-        // A를 B번 곱한 수를 C로 나눈 나머지 계산
-        long result = powerMod(A, B, C);
-        System.out.println(result);
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException{
+        String [] tokens = br.readLine().split(" ");
+        long A = Long.parseLong(tokens[0]);
+        long B = Long.parseLong(tokens[1]);
+        long C = Long.parseLong(tokens[2]);
         
-        scanner.close();
+        long result = multiple(A%C, B, C);
+        bw.write(Long.toString(result)+"\n");
+        bw.flush();
+        bw.close();
     }
-
-    // 거듭제곱을 분할 정복을 이용하여 효율적으로 계산하는 함수
-    public static long powerMod(long A, long B, long C) {
-        if (B == 0) {
-            return 1 % C;
-        } else if (B % 2 == 0) {
-            long temp = powerMod(A, B / 2, C);
-            return (temp * temp) % C;
-        } else {
-            long temp = powerMod(A, B - 1, C);
-            return (A * temp) % C;
+    public static long multiple(long a, long b, long c){
+        if(b == 0){
+            return 1%c;
+        }else if(b%2 == 0){
+            long temp = multiple(a, b/2, c);
+            return (temp*temp)%c;
+        }else{
+            long temp = multiple(a, b-1, c);
+            return (a*temp)%c;
         }
     }
 }
