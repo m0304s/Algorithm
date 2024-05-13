c827b8ae26ee5a599dc5836c6478ce5e21dc9664
MinSeok_Choi <120546936+m0304s@users.noreply.github.com>
2024-05-13 14:01:50 +0900

    [Silver III] Title: N과 M (1), Time: 2088 ms, Memory: 61728 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+public class Main {
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static int [] arr;
+    public static boolean [] visited;
+    public static void main(String[] args) throws IOException{
+        String [] tokens = br.readLine().split(" ");
+        int N = Integer.parseInt(tokens[0]);
+        int M = Integer.parseInt(tokens[1]);
+
+        arr = new int[M];
+        visited = new boolean[N];
+
+        dfs(N,M,0);
+        bw.flush();
+    }
+    public static void dfs(int N, int M, int depth) {
+		if (depth == M) {
+			for (int val : arr) {
+				System.out.print(val + " ");
+			}
+			System.out.println();
+			return;
+		}
+ 
+		for (int i = 0; i < N; i++) {
+			if (!visited[i]) {
+				visited[i] = true;
+				arr[depth] = i + 1;
+				dfs(N, M, depth + 1);
+				visited[i] = false;
+			}
+		}
+    }
+}
