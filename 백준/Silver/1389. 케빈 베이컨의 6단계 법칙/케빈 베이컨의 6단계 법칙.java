496b449fb900c3ed84ccf6ea0096c741055cb83f
MinSeok_Choi <120546936+m0304s@users.noreply.github.com>
2024-07-05 14:29:06 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 156 ms, Memory: 17428 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -5,12 +5,18 @@ public class Main {
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    static int[] dx = {0,0,-1,1};
-    static int[] dy = {-1,1,0,0};
     static boolean[][] connections;
     static boolean[] visited;
     static int N;
     static int M;
+    static class Node{
+        int target;
+        int dept;
+        public Node(int target,int dept){
+            this.target = target;
+            this.dept = dept;
+        }
+    }
     public static void main(String[] args) throws IOException{
         String[] tokens = br.readLine().split(" ");
         N = Integer.parseInt(tokens[0]);
@@ -32,12 +38,9 @@ public class Main {
 
         for(int i=1;i<=N;i++){
             int count = 0;
-            for(int j=1;j<=N;j++){
-                if(i==j) continue;
-                visited = new boolean[N+1];
-                visited[i] = true;
-                count+=bfs(i,j);
-            }
+            visited = new boolean[N+1];
+            visited[i] = true;
+            count+=bfs(i);
             result.add(count);
         }
         int min = result.get(0);
@@ -51,30 +54,30 @@ public class Main {
         bw.write(ans+1+"\n");
         bw.flush();
     }
-    public static int bfs(int from, int to){
-        Queue<Integer> queue = new LinkedList<>();
-        for(int j=1;j<=N;j++){
-            if(connections[from][j]){
-                visited[j] = true;
-                queue.add(j);
+    public static int bfs(int from){
+        int total = 0;
+        Queue<Node> queue = new LinkedList<>();
+        for(int i=1;i<=N;i++){
+            if(connections[from][i]){
+                Node node = new Node(i, 1);
+                queue.add(node);
             }
         }
-        int cnt=1;
-		while(!queue.isEmpty()) {
-			int qSize=queue.size();
-			while(qSize-->0) {
-				int num=queue.poll();
-				if(num==to) {
-					return cnt;
-				}
-				for(int j=1; j<=N; j++) {
-					if(visited[j]==true || !connections[num][j]) continue;
-					visited[j]=true;
-					queue.offer(j);
-				}
-			}
-			cnt++;
-		}
-        return 0;
+        while (!queue.isEmpty()) {
+            Node now = queue.poll();
+            if(visited[now.target]){
+                continue;
+            }
+            visited[now.target] = true;
+            total+=now.dept;
+            
+            for(int i=1;i<=N;i++){
+                if(connections[now.target][i]){
+                    Node new_node = new Node(i, now.dept+1);
+                    queue.add(new_node);
+                }
+            }
+        }
+        return total;
     }
 }
