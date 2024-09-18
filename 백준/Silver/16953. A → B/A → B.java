4ce6fa97ee925aed2e4a153fd80069442a4a9d94
MinSeok_Choi <120546936+m0304s@users.noreply.github.com>
2024-09-18 16:35:59 +0900

    [Silver II] Title: A → B, Time: 184 ms, Memory: 24848 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.java
+++ 백준/Silver/16953. A → B/A → B.java
@@ -8,41 +8,38 @@ public class Main {
     static Long startNum;
     static Long targetNum;
     static Set<Long> visited = new HashSet<>();
-
-    public static void main(String[] args) throws IOException {
-        String[] tokens = br.readLine().split(" ");
+    public static void main(String[] args) throws IOException{
+        String [] tokens = br.readLine().split(" ");
         startNum = Long.parseLong(tokens[0]);
         targetNum = Long.parseLong(tokens[1]);
         bfs();
         bw.flush();
     }
-
-    static void bfs() throws IOException {
+    static void bfs() throws IOException{
         Queue<Long> queue = new LinkedList<>();
         queue.add(startNum);
         visited.add(startNum);
 
         int answer = 0;
-        while (!queue.isEmpty()) {
+        while(!queue.isEmpty()){
             answer++;
             int size = queue.size();
-            for (int i = 0; i < size; i++) {
+            for(int i=0;i<size;i++){
                 Long nowNum = queue.poll();
-                if (nowNum.equals(targetNum)) {
-                    bw.write(answer + "\n");
+                if(nowNum.equals(targetNum)){
+                    System.out.println(answer);
                     return;
                 }
-
-                if (nowNum * 2 <= targetNum && !visited.contains(nowNum * 2)) {
-                    queue.add(nowNum * 2);
-                    visited.add(nowNum * 2);
+                if(nowNum*2<=targetNum && !visited.contains(nowNum*2)){
+                    queue.add(nowNum*2);
+                    visited.add(nowNum*2);
                 }
-                if (nowNum * 10 + 1 <= targetNum && !visited.contains(nowNum * 10 + 1)) {
-                    queue.add(nowNum * 10 + 1);
-                    visited.add(nowNum * 10 + 1);
+                if((nowNum*10+1) <= targetNum && !visited.contains(nowNum*10+1)){
+                    queue.add(nowNum*10+1);
+                    visited.add(nowNum*10+1);
                 }
             }
         }
-        bw.write("-1\n");
+        System.out.println(-1);
     }
 }
