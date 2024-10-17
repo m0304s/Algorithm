4c243d83a8759ff7ebe1f164db0478849e88e5b5
MinSeok_Choi <120546936+m0304s@users.noreply.github.com>
2024-10-17 15:45:40 +0900

    [D3] Title: 부분 수열의 합, Time: 224 ms, Memory: 19,744 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
@@ -7,42 +7,43 @@ public class Solution {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     static int[] nums;
-    static int N, K;
+    static boolean[] visited;
+    static int N,K;
     static List<Integer> array = new ArrayList<>();
     static int answer = 0;
 
     public static void main(String[] args) throws IOException {
         int T = Integer.parseInt(br.readLine());
-        for (int t = 1; t <= T; t++) {
-            String[] tokens = br.readLine().split(" ");
+        for(int t=1;t<=T;t++){
+            String [] tokens = br.readLine().split(" ");
             N = Integer.parseInt(tokens[0]);
             K = Integer.parseInt(tokens[1]);
             nums = new int[N];
+            visited = new boolean[N];
 
             tokens = br.readLine().split(" ");
-            for (int i = 0; i < N; i++) {
+            for(int i=0;i<N;i++){
                 nums[i] = Integer.parseInt(tokens[i]);
             }
-
             answer = 0;
-            backtracking(0, 0);
-            bw.write("#" + t + " " + answer + "\n");
+            backtracking(0,0);
+            bw.write("#"+t+" "+answer+"\n");
         }
         bw.flush();
         bw.close();
         br.close();
     }
 
-    static void backtracking(int index, int sum) {
-        if (sum == K) {
+    static void backtracking(int index, int sum){
+        if(sum == K){
             answer++;
         }
-
-        for (int i = index; i < nums.length; i++) {
-            array.add(nums[i]);
-            // 다음 탐색은 i + 1부터 시작
-            backtracking(i + 1, sum + nums[i]);
-            array.remove(array.size() - 1); // 마지막 요소 제거 (백트래킹)
+        for(int i=index;i< nums.length;i++){
+            if(!visited[i]){
+                visited[i] = true;
+                backtracking(i+1,sum+nums[i]);
+                visited[i] = false;
+            }
         }
     }
 }
