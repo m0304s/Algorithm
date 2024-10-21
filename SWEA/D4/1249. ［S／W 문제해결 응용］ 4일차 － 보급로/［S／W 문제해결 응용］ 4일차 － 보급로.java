d34c6ddbf63106fa3f4d2c2ec94788ce3551e402
MinSeok_Choi <120546936+m0304s@users.noreply.github.com>
2024-10-21 12:09:04 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 145 ms, Memory: 24,132 KB -BaekjoonHub

---
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -0,0 +1,75 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N;
+    static int [][] map;
+    static int [][] dist;
+
+    static int [] dx = {-1,1,0,0};
+    static int [] dy = {0,0,-1,1};
+
+    static class Node implements Comparable<Node>{
+        int x,y,distance;
+        public Node(int x,int y,int distance){
+            this.x = x;
+            this.y = y;
+            this.distance = distance;
+        }
+        public int compareTo(Node o){
+            return this.distance - o.distance;
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        int T = Integer.parseInt(br.readLine());
+        for(int t=1;t<=T;t++){
+            N = Integer.parseInt(br.readLine());
+            map = new int[N][N];
+            dist = new int[N][N];
+
+            for(int i=0;i<N;i++){
+                String input = br.readLine();
+                for(int j=0;j<N;j++){
+                    map[i][j] = input.charAt(j)-'0';
+                }
+            }
+            for (int[] ints : dist) {
+                Arrays.fill(ints,Integer.MAX_VALUE);
+            }
+            dist[0][0] = 0;
+            dijkstra();
+            bw.write("#"+t+" "+dist[N-1][N-1]+"\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+    static void dijkstra(){
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(0,0,0));
+
+        while(!pq.isEmpty()){
+            Node cur = pq.poll();
+
+            for(int i=0;i<4;i++){
+                int nx = cur.x+dx[i];
+                int ny = cur.y+dy[i];
+                if(!inRange(nx,ny)) continue;
+
+                int nd = cur.distance + map[nx][ny];
+
+                if(dist[nx][ny] > nd){
+                    dist[nx][ny] = nd;
+                    pq.add(new Node(nx,ny,nd));
+                }
+            }
+        }
+    }
+
+    static boolean inRange(int x,int y){
+        return x>=0 && x<N && y>=0 && y<N;
+    }
+}
