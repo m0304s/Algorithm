862fead3f24fc83d9ad46e888c2a43d503a97276
MinSeok_Choi <120546936+m0304s@users.noreply.github.com>
2024-11-12 11:43:10 +0900

    [Silver II] Title: 촌수계산, Time: 112 ms, Memory: 14604 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -1,52 +1,65 @@
 import java.io.*;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
-public class Main{
-    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    public static void main(String[] args) throws IOException{
-        int N = Integer.parseInt(br.readLine());    //전체 사람의 수
-        String [] tokens = br.readLine().split(" ");
-        int n1 = Integer.parseInt(tokens[0]);   //첫번째 사람의 번호
-        int n2 = Integer.parseInt(tokens[1]);   //두번째 사람의 번호
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int [][] wires = new int[N+1][N+1];
-        boolean [] visited = new boolean[N+1];
+    static int N, M, START, TARGET;
+    static int[][] graph;
 
-        int M = Integer.parseInt(br.readLine());
-        for(int i=0;i<M;i++){
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        graph = new int[N + 1][N + 1];
+        String[] tokens = br.readLine().split(" ");
+        START = Integer.parseInt(tokens[0]);
+        TARGET = Integer.parseInt(tokens[1]);
+
+        M = Integer.parseInt(br.readLine());
+        for (int i = 0; i < M; i++) {
             tokens = br.readLine().split(" ");
             int v1 = Integer.parseInt(tokens[0]);
             int v2 = Integer.parseInt(tokens[1]);
 
-            wires[v1][v2] = wires[v2][v1] = 1;
+            graph[v1][v2] = 1;
+            graph[v2][v1] = 1;
         }
-        int count = bfs(n1,n2, wires, visited);
-        System.out.println(count);
-    }
-    public static int bfs(int v1,int v2, int[][] wires, boolean[] visited){
-        Queue<int[]> q = new LinkedList<>();
 
-        q.add(new int[]{v1, 0});
-        visited[v1] = true;
+        int answer = bfs();
+        bw.write(answer + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    private static int bfs() {
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[N + 1];
+        visited[START] = true;
+        queue.add(START);
+
         int count = 0;
 
-        while(!q.isEmpty()){
-            int[] temp = q.poll();
-            if(temp[0]==v2){
-                count = temp[1];   //친척 관계가 있는 경우
-                break;
-            }
-            for(int i=0;i<wires.length;i++){
-                if(wires[temp[0]][i]==1 && !visited[i]){
-                    q.add(new int[]{i, temp[1] + 1});
-                    visited[i] = true;
+        while (!queue.isEmpty()) {
+            int size = queue.size(); // Nodes at the current level
+
+            for (int i = 0; i < size; i++) {
+                int node = queue.poll();
+
+                if (node == TARGET) {
+                    return count;
+                }
+
+                for (int j = 0; j < graph.length; j++) {
+                    if (graph[node][j] == 1 && !visited[j]) {
+                        queue.add(j);
+                        visited[j] = true;
+                    }
                 }
             }
+            count++;
         }
-        if(count == 0){
-            return -1;
-        }
-        return count;
+        return -1;
     }
 }
