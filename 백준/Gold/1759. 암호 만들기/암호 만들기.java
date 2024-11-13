771aa6c8b036e3b3002c86761ef7d6d1abe5fcee
MinSeok_Choi <120546936+m0304s@users.noreply.github.com>
2024-11-13 14:01:28 +0900

    [Gold V] Title: 암호 만들기, Time: 136 ms, Memory: 19748 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static int L,C;
+    static String[] alphabets;
+    static String[] output;
+    static ArrayList<String> answers = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        String [] tokens = br.readLine().split(" ");
+        L = Integer.parseInt(tokens[0]);
+        C = Integer.parseInt(tokens[1]);
+        alphabets = new String[C];
+        output = new String[L];
+        tokens = br.readLine().split(" ");
+        for(int i=0;i<C;i++){
+            alphabets[i] = tokens[i];
+        }
+        Arrays.sort(alphabets);
+        backtracking(0,0,0,0,"");
+        Collections.sort(answers);
+        for (String answer : answers) {
+            System.out.println(answer);
+        }
+    }
+    private static void backtracking(int depth, int index,int vowels, int consonants,String password){
+        if(depth == L){
+            if(vowels >= 1 && consonants >= 2 ){
+                answers.add(password);
+            }
+            return;
+        }
+
+        if(index >= C) return;
+
+        char currentChar = alphabets[index].charAt(0);
+        if(isVowel(currentChar)){
+            backtracking(depth+1,index+1,vowels+1,consonants,password+alphabets[index]);
+        }else{
+            backtracking(depth+1,index+1,vowels,consonants+1,password+alphabets[index]);
+        }
+        //현재 보고 있는 character를 스킵
+        backtracking(depth,index+1,vowels,consonants,password);
+    }
+    private static boolean isVowel(char ch){
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
+}
