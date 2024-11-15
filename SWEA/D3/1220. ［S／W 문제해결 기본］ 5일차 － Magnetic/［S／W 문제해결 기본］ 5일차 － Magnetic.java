f4815afa4aa172ec61e7b04b9d1f6313720f8de9
MinSeok_Choi <120546936+m0304s@users.noreply.github.com>
2024-11-15 10:57:03 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - Magnetic, Time: 159 ms, Memory: 31,476 KB -BaekjoonHub

--- SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.java
+++ SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.java
@@ -4,40 +4,42 @@ public class Solution {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+    static int[][] map;
     static final int N = 1;
     static final int S = 2;
-    static final int BLANK = 0;
 
-    static int [][] map = new int[100][100];
-
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         for(int t=1;t<=10;t++){
-            int N = Integer.parseInt(br.readLine());    //정사각형의 한변의 길이
-            for(int i=0;i<100;i++){
-                String [] nums = br.readLine().split(" ");
-                for(int j=0;j<100;j++){
-                    map[i][j] = Integer.parseInt(nums[j]);
+            int mapLength = Integer.parseInt(br.readLine());
+            map = new int[mapLength][mapLength];
+
+            for(int i=0;i<mapLength;i++){
+                String [] tokens = br.readLine().split(" ");
+                for(int j=0;j<mapLength;j++){
+                    map[i][j] = Integer.parseInt(tokens[j]);
                 }
             }
-            int count = findDeadLock();
-            bw.write("#"+t+" "+count+"\n");
+
+            int deadLockCnt = findDeadLock();
+
+            bw.write("#"+t+" "+deadLockCnt+"\n");
         }
         bw.flush();
         bw.close();
         br.close();
     }
-    static int findDeadLock(){
+
+    private static int findDeadLock(){
         int count = 0;
 
-        // 세로부터 시작
-        for(int i=0;i<map[0].length;i++){
+        for(int j=0;j<map.length;j++){
             boolean state = false;
-            for(int j=0;j<map.length;j++){
-                if(map[j][i] == N) state = true;
+            for(int i=0;i<map.length;i++){
+                if(map[i][j] == N){
+                    state = true;
+                }
 
-                //만약 이전에 N극이 있던 상태에서, S극을 만나면 count +1 후 state 초기화
-                if(state && map[j][i] == S){
+                if(state && map[i][j] == S){
                     count++;
                     state = false;
                 }
