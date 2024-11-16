ccbafb9764a55b6e7505e092e466bbd6588f9d75
MinSeok_Choi <120546936+m0304s@users.noreply.github.com>
2024-11-16 16:11:28 +0900

    [D3] Title: 햄버거 다이어트, Time: 222 ms, Memory: 20,352 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,50 +1,50 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class Solution {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    static int [][] ingredients;
-    static int N;
-    static int totalCalories;
-    static int answer;
+    static int[] costs;
+    static int[] calories;
 
-    static boolean [] visited;
-    public static void main(String[] args) throws IOException{
+    static int N,L;
+    static int maxCost;
+    static int[] output;
+
+    public static void main(String[] args) throws IOException {
         int T = Integer.parseInt(br.readLine());
         for(int t=1;t<=T;t++){
-            answer = 0;
+            maxCost = Integer.MIN_VALUE;
             String [] tokens = br.readLine().split(" ");
             N = Integer.parseInt(tokens[0]);
-            totalCalories = Integer.parseInt(tokens[1]);
-            ingredients = new int[N][2];
+            L = Integer.parseInt(tokens[1]);
+
+            costs = new int[N];
+            calories = new int[N];
+            output = new int[N];
+
             for(int i=0;i<N;i++){
                 tokens = br.readLine().split(" ");
-                ingredients[i][0] = Integer.parseInt(tokens[0]);
-                ingredients[i][1] = Integer.parseInt(tokens[1]);
+                costs[i] = Integer.parseInt(tokens[0]);
+                calories[i] = Integer.parseInt(tokens[1]);
             }
-            dfs(0,0, 0);
-            bw.write("#"+t+" " + answer+"\n");
+
+            combination(0,0,0);
+            bw.write("#"+t+" "+maxCost+"\n");
         }
         bw.flush();
         bw.close();
         br.close();
     }
-
-    static void dfs(int index,int currentScore, int currentCalories){
-        if(currentCalories > totalCalories) return;
-
+    private static void combination(int index, int totalCost, int totalCalories){
         if(index == N){
-            answer = Math.max(currentScore, answer);
+            if(totalCalories<=L){
+                maxCost = Math.max(maxCost,totalCost);
+            }
             return;
         }
 
-        dfs(index+1,currentScore + ingredients[index][0], currentCalories + ingredients[index][1]); //현재 재료를 선택하는 경우
-        dfs(index+1, currentScore, currentCalories);  //현재 재료를 선택하지 않는 경우
+        combination(index+1,totalCost,totalCalories);
+        combination(index+1,totalCost+costs[index],totalCalories+calories[index]);
     }
-    
 }
