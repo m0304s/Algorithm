c8cc5bd9598d59bb1d39bdfecdac9401b234dc67
MinSeok_Choi <120546936+m0304s@users.noreply.github.com>
2024-11-17 11:28:57 +0900

    [D4] Title: 동철이의 일 분배, Time: 2,444 ms, Memory: 22,700 KB -BaekjoonHub

--- SWEA/D4/1865. 동철이의 일 분배/동철이의 일 분배.java
+++ SWEA/D4/1865. 동철이의 일 분배/동철이의 일 분배.java
@@ -1,57 +1,49 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class Solution {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    static double maxPossibility;
-
-    static int [][] possibility;
-    static double [] output;
-    static boolean [] visited;
     static int N;
-    public static void main(String[] args) throws IOException {
+    static int [][] probability;
+    static double maxProbability;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException{
         int T = Integer.parseInt(br.readLine());
         for(int t=1;t<=T;t++){
             N = Integer.parseInt(br.readLine());
-            output = new double[N];
+            probability = new int[N][N];
             visited = new boolean[N];
-            maxPossibility = Double.MIN_VALUE;
-            possibility = new int[N][N];
+            maxProbability = Double.MIN_VALUE;
             for(int i=0;i<N;i++){
                 String [] tokens = br.readLine().split(" ");
                 for(int j=0;j<N;j++){
-                    possibility[i][j] = Integer.parseInt(tokens[j]);
+                    probability[i][j] = Integer.parseInt(tokens[j]);
                 }
             }
-            dfs(0,1.0);
-//            bw.write("#"+t+" "+String.format("%.6f",maxPossibility*100));
-            System.out.println("#"+t+" "+String.format("%.6f",maxPossibility*100));
+
+            permutation(0,1.0);
+            bw.write("#"+t+" "+String.format("%.6f",maxProbability*100)+"\n");
         }
         bw.flush();
         bw.close();
         br.close();
     }
-    private static void dfs(int depth,double currentPossibility){
+    private static void permutation(int depth, double currentProbability){
         if(depth == N){
-            maxPossibility = Math.max(currentPossibility,maxPossibility);
+            maxProbability = Math.max(maxProbability,currentProbability);
             return;
         }
-        if (currentPossibility <= maxPossibility) {
-            return;
-        }
+        if(currentProbability<maxProbability) return;
         for(int i=0;i<N;i++){
             if(visited[i]) continue;
             visited[i] = true;
-            dfs(depth+1,currentPossibility*toDouble(possibility[depth][i]));
+            permutation(depth+1,currentProbability*toDouble(probability[depth][i]));
             visited[i] = false;
         }
     }
-    private static double toDouble(int num){
-        return num/100.0;
+    private static double toDouble(int i){
+        return i/100.0;
     }
 }
