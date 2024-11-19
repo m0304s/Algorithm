63bb48f0c7c2b19064d1b8d6c5ec577ec9ef0b3f
MinSeok_Choi <120546936+m0304s@users.noreply.github.com>
2024-11-19 17:39:54 +0900

    [Gold III] Title: 아기 상어, Time: 136 ms, Memory: 18272 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -1,45 +1,45 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     static int N;
     static int [][] ocean;
-    static final int BABY_SHARK = 9;
 
+    //상 하 좌 우
     static int [] dx = {-1,1,0,0};
     static int [] dy = {0,0,-1,1};
 
     static class Shark{
-        int x,y,size,eatCount;
+        int x,y,size,countToEat;
 
         public Shark(int x,int y){
             this.x = x;
             this.y = y;
             this.size = 2;
-            this.eatCount = 0;
+            this.countToEat = 0;
         }
 
         public void eat(){
-            this.eatCount++;
-            if(this.eatCount == this.size){
-                eatCount = 0;
-                increaseSize();
+            countToEat++;
+            if(countToEat == size){
+                size++;
+                countToEat = 0;
             }
-        }
-
-        public void increaseSize(){
-            this.size++;
         }
     }
 
+    static Shark shark;
+
     static class Node{
-        int x;
-        int y;
-        int distance;
+        int x,y,distance;
 
         public Node(int x,int y,int distance){
             this.x = x;
@@ -48,7 +48,6 @@ public class Main {
         }
     }
 
-    static Shark shark;
     public static void main(String[] args) throws IOException{
         N = Integer.parseInt(br.readLine());
         ocean = new int[N][N];
@@ -57,47 +56,45 @@ public class Main {
             String [] tokens = br.readLine().split(" ");
             for(int j=0;j<N;j++){
                 ocean[i][j] = Integer.parseInt(tokens[j]);
-                if(ocean[i][j] == BABY_SHARK) {
-                    shark = new Shark(i,j);
-                    ocean[i][j] = 0;
+                if (ocean[i][j] == 9) {
+                    shark = new Shark(i, j);
+                    ocean[i][j] = 0; // 초기 위치 값 제거
                 }
             }
         }
-
-        bw.write(simulate()+"\n");
-        bw.flush();
-        bw.close();
-        br.close();
+        int time = simulation();
+        System.out.println(time);
     }
-    private static int simulate(){
+
+    private static int simulation(){
         int time = 0;
-
-        while(true){
+        while (true){
             Node fishToEat = findClosestFish();
-            if(fishToEat == null) break;
-
-            int distance = fishToEat.distance;
-            time+=distance;
-
+            if(fishToEat == null){
+                break;
+            }
+            time+=fishToEat.distance;
             shark.x = fishToEat.x;
             shark.y = fishToEat.y;
-
-            shark.eat();
             ocean[shark.x][shark.y] = 0;
+            shark.eat();
         }
         return time;
     }
 
     private static Node findClosestFish() {
+        Node fishToEat = null;
+        int minDistance = Integer.MAX_VALUE;
+        int startX = shark.x;
+        int startY = shark.y;
+
         Queue<Node> queue = new LinkedList<>();
         boolean[][] visited = new boolean[N][N];
 
-        queue.add(new Node(shark.x,shark.y,0));
-        visited[shark.x][shark.y] = true;
+        visited[startX][startY] = true;
+        queue.add(new Node(startX,startY,0));
 
-        Node closestFish  = null;
-        int minDistance = Integer.MAX_VALUE;
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()){
             Node cur = queue.poll();
 
             for(int i=0;i<4;i++){
@@ -109,24 +106,26 @@ public class Main {
                 if(ocean[nx][ny] > shark.size) continue;
 
                 visited[nx][ny] = true;
-                int newDistance = cur.distance +1;
+                int newDistance = cur.distance + 1;
 
-                //가장 가까운 물고기 선택
                 if(ocean[nx][ny] > 0 && ocean[nx][ny] < shark.size){
-                    if(newDistance < minDistance ||
-                            (newDistance == minDistance && (nx < closestFish.x || (nx == closestFish.x && ny < closestFish.y)))) {
+                    if (fishToEat == null ||
+                            newDistance < minDistance ||
+                            (newDistance == minDistance && (nx < fishToEat.x || (nx == fishToEat.x && ny < fishToEat.y)))
+                    ) {
                         minDistance = newDistance;
-                        closestFish = new Node(nx,ny,newDistance);
+                        fishToEat = new Node(nx, ny, newDistance);
                     }
                 }else{
                     queue.add(new Node(nx,ny,newDistance));
                 }
             }
         }
-        return closestFish;
+        return fishToEat;
     }
 
+
     private static boolean inRange(int x,int y){
-        return x >=0 && x < N && y >= 0 && y < N;
+        return x >= 0 && x < N && y >= 0 && y < N;
     }
 }
