afade3b90bbca5dced1da1daade3219367e912da
MinSeok_Choi <120546936+m0304s@users.noreply.github.com>
2024-12-08 18:32:56 +0900

    [Gold I] Title: 2048 (Easy), Time: 212 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
@@ -3,9 +3,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
