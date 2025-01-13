7f20feb4920258b24fbe8309356afebfa02174ee
MinSeok_Choi <120546936+m0304s@users.noreply.github.com>
2025-01-13 22:28:58 +0900

    [Gold V] Title: MooTube (Silver), Time: 1168 ms, Memory: 302600 KB -BaekjoonHub

--- 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.java
+++ 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.java
@@ -1,81 +1,77 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
-	static int n, q;
-	static ArrayList <Node> al[];
-	static StringBuilder sb = new StringBuilder();
+class Main
+{
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	
-	public static void main(String [] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+	private static int N,Q;
+	private static class Node{
+		int node,weight;
 		
-		st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		q = Integer.parseInt(st.nextToken());
-		
-		al = new ArrayList[n];
-		
-		for(int i = 0; i < n; i++) {
-			al[i] = new ArrayList<>();
+		public Node(int node, int weight) {
+			this.node = node;
+			this.weight = weight;
 		}
-		
-		for(int i = 0; i < n-1; i++) {
-			st = new StringTokenizer(br.readLine());
-			int start = Integer.parseInt(st.nextToken());
-			int next = Integer.parseInt(st.nextToken());
-			int usado = Integer.parseInt(st.nextToken());
-			
-			al[start-1].add(new Node(next-1, usado));
-			al[next-1].add(new Node(start-1, usado));
-		}
-		
-		for(int i = 0; i < q; i++) {
-			st = new StringTokenizer(br.readLine());
-			int k = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			sb.append(findUsado(k,v-1) + "\n");
-		}
-		
-		System.out.print(sb);
 	}
 	
-	private static int findUsado (int k, int idx) {
-		int min = 0;
-		int cnt = 0;
+	public static void main(String args[]) throws Exception
+	{
+		String [] tokens = br.readLine().split(" ");
+		N = Integer.parseInt(tokens[0]);
+		Q = Integer.parseInt(tokens[1]);
+		ArrayList<ArrayList<Node>> graph = new ArrayList<>();
 		
-		boolean visited[] = new boolean[n];
-		visited[idx] = true;
+		for(int i=0;i<N;i++) {
+			graph.add(new ArrayList<>());
+		}
 		
-		Queue<Node> q = new LinkedList<>();
-		
-		q.add(new Node(idx, Integer.MAX_VALUE));
-		while(!q.isEmpty()) {
-			Node node = q.poll();
+		for(int i=0;i<N-1;i++) {
+			tokens = br.readLine().split(" ");	//유사도 입력
+			int p = Integer.parseInt(tokens[0])-1;
+			int q = Integer.parseInt(tokens[1])-1;
+			int r = Integer.parseInt(tokens[2]);
 			
-			for(Node next : al[node.now]) {
-				if(!visited[next.now]) {
-					visited[next.now] = true;
-					int newUsado = Math.min(next.usado, node.usado);
-					if(newUsado >= k) {
-						q.add(new Node(next.now, newUsado));
-						cnt++;
-					}
-				}
+			graph.get(p).add(new Node(q,r));
+			graph.get(q).add(new Node(p,r));
+		}
+		
+		for(int i=0;i<Q;i++) {
+			tokens = br.readLine().split(" ");
+			int k = Integer.parseInt(tokens[0]);
+			int v = Integer.parseInt(tokens[1])-1;
+			
+			int answer = bfs(k,v,graph);
+			bw.write(answer+"\n");
+		}
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+	
+	private static int bfs(int k,int v,ArrayList<ArrayList<Node>> graph) {
+		boolean [] visited = new boolean[N];
+		Queue<Integer> queue = new LinkedList<>();
+		visited[v] = true;
+		queue.add(v);
+		
+		int count=0;
+		
+		while(!queue.isEmpty()) {
+			int curNode = queue.poll();
+			for(int i=0;i<graph.get(curNode).size();i++) {
+				Node node = graph.get(curNode).get(i);
+				int n = node.node;
+				int w = node.weight;
+				
+				if(w<k || visited[n]) continue;
+				
+				count++;
+				queue.add(n);
+				visited[n] = true;
 			}
 		}
-		
-		return cnt;
-	}
-	
-	
-	
-	static class Node{
-		int now;
-		int usado;
-		Node(int now, int usado){
-			this.now = now;
-			this.usado = usado;
-		}
+		return count;
 	}
 }
