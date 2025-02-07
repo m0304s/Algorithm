9b1219972d4b621569497e7938df4bf673c966d7
MinSeok_Choi <120546936+m0304s@users.noreply.github.com>
2025-02-07 21:26:21 +0900

    [Gold II] Title: 미확인 도착지, Time: 588 ms, Memory: 73924 KB -BaekjoonHub

--- 백준/Gold/9370. 미확인 도착지/README.md
+++ 백준/Gold/9370. 미확인 도착지/README.md
@@ -4,7 +4,7 @@
 
 ### 성능 요약
 
-메모리: 87084 KB, 시간: 664 ms
+메모리: 73924 KB, 시간: 588 ms
 
 ### 분류
 
@@ -12,7 +12,7 @@
 
 ### 제출 일자
 
-2023년 8월 6일 18:32:17
+2025년 2월 7일 21:26:11
 
 ### 문제 설명
 
@@ -22,7 +22,7 @@
 
 <p>이 듀오는 대체 어디로 가고 있는 것일까?</p>
 
-<p style="text-align: center;"><img alt="" src="" style="font-size:medium; height:230px; width:236px"></p>
+<p style="text-align: center;"><img alt="" src="https://www.acmicpc.net/upload/images/destination.png" style="font-size:medium; height:230px; width:236px"></p>
 
 <p>예제 입력의 두 번째 케이스를 시각화한 것이다. 이 듀오는 회색 원에서 두 검은 원 중 하나로 가고 있고 점선으로 표시된 도로에서 냄새를 맡았다. 따라서 그들은 6으로 향하고 있다.</p>
 
--- 백준/Gold/9370. 미확인 도착지/미확인 도착지.java
+++ 백준/Gold/9370. 미확인 도착지/미확인 도착지.java
@@ -2,109 +2,105 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static class Node implements Comparable<Node>{
-        int end, weight;
+    private static class Node implements Comparable<Node> {
+        int idx, cost;
 
-        public Node(int end, int weight) {
-            this.end = end;
-            this.weight = weight;
+        public Node(int idx, int cost) {
+            this.idx = idx;
+            this.cost = cost;
         }
 
-        @Override
         public int compareTo(Node o) {
-            return weight - o.weight;
+            return this.cost - o.cost;
         }
     }
+
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    private static final int INF = 10_000_000;
-    static int vertex, edge, t;
-    static int start, g, h;
-    static int arr[][];
-    static int dist[];
-    static boolean check[];
-    static List<Integer> answerList;
-
+    private static int N, M, T, S, G, H;
+    private static final int INF = 1000000000;
+    private static List<List<Node>> graph;
+    private static List<Integer> possibleTargets;
 
     public static void main(String[] args) throws IOException {
-        int testCnt = Integer.parseInt(br.readLine());
+        int CASE = Integer.parseInt(br.readLine());
 
-        for(int i = 0 ; i < testCnt; i++){
-            // n,m,t 초기화
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            vertex = Integer.parseInt(st.nextToken());
-            edge = Integer.parseInt(st.nextToken());
-            t = Integer.parseInt(st.nextToken());
+        for (int x = 0; x < CASE; x++) {
+            String[] tokens = br.readLine().split(" ");
+            N = Integer.parseInt(tokens[0]);
+            M = Integer.parseInt(tokens[1]);
+            T = Integer.parseInt(tokens[2]);
 
-            // 그래프 배열 선언
-            arr = new int[vertex + 1][vertex + 1];
-            dist = new int[vertex + 1];
-            for(int j = 0 ; j < arr.length; j++)
-                Arrays.fill(arr[j], INF);
-            Arrays.fill(dist, INF);
-            check = new boolean[vertex + 1];
+            tokens = br.readLine().split(" ");
+            S = Integer.parseInt(tokens[0]);
+            G = Integer.parseInt(tokens[1]);
+            H = Integer.parseInt(tokens[2]);
 
-            // s, g, h 초기화
-            st = new StringTokenizer(br.readLine());
-            start = Integer.parseInt(st.nextToken());
-            g = Integer.parseInt(st.nextToken());
-            h = Integer.parseInt(st.nextToken());
+            graph = new ArrayList<>();
+            possibleTargets = new ArrayList<>();
 
-            // 그래프 정보 저장
-            for(int j = 0 ; j < edge; j++){
-                st = new StringTokenizer(br.readLine());
-                int vertex1 = Integer.parseInt(st.nextToken());
-                int vertex2 = Integer.parseInt(st.nextToken());
-                int distance = Integer.parseInt(st.nextToken());
-                // 2배의 가중치를 저장
-                arr[vertex1][vertex2] = arr[vertex2][vertex1] = distance * 2;
+            for (int i = 0; i <= N; i++) {
+                graph.add(new ArrayList<>());
             }
-            // 2배된 값에 -1을 하여 홀수를 만든다.
-            arr[h][g] = arr[g][h] = arr[h][g] - 1;
 
-            // 후보정답 list선언
-            answerList = new ArrayList<>();
-            // 후보가 되는 값 추가
-            for(int j = 0; j < t; j++)
-                answerList.add(Integer.parseInt(br.readLine()));
+            for (int i = 0; i < M; i++) {
+                tokens = br.readLine().split(" ");
+                int a = Integer.parseInt(tokens[0]);
+                int b = Integer.parseInt(tokens[1]);
+                int d = Integer.parseInt(tokens[2]);
 
-            solve();
-            // 오름차순 정렬
-            Collections.sort(answerList);
-            // 정답 출력
-            for(int num : answerList)
-                if(dist[num] % 2 == 1) bw.write(num + " ");
+                graph.get(a).add(new Node(b, d));
+                graph.get(b).add(new Node(a, d));
+            }
+
+            for (int i = 0; i < T; i++) {
+                possibleTargets.add(Integer.parseInt(br.readLine()));
+            }
+
+            int[] distFromS = dijkstra(S);
+            int[] distFromG = dijkstra(G);
+            int[] distFromH = dijkstra(H);
+
+            List<Integer> answer = new ArrayList<>();
+            for (int target : possibleTargets) {
+                if (distFromS[target] == INF) continue;
+                int throughGH = Math.min(distFromS[G] + distFromG[H] + distFromH[target],
+                                         distFromS[H] + distFromH[G] + distFromG[target]);
+                if (distFromS[target] == throughGH) {
+                    answer.add(target);
+                }
+            }
+
+            Collections.sort(answer);
+            for (int num : answer) {
+                bw.write(num + " ");
+            }
             bw.write("\n");
         }
-
+        bw.flush();
         bw.close();
         br.close();
     }
 
-    private static void solve() {
-        dijkstra();
-    }
-
-    private static void dijkstra(){
-        PriorityQueue<Node> queue = new PriorityQueue<>();
-        queue.add(new Node(start, 0));
+    private static int[] dijkstra(int start) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        int[] dist = new int[N + 1];
+        Arrays.fill(dist, INF);
         dist[start] = 0;
+        pq.add(new Node(start, 0));
 
-        // 큐가 빌 때까지
-        while(!queue.isEmpty()){
-            Node curNode = queue.poll();
-            int cur = curNode.end;
-            // 이미 방문한 노드인 경우 패스
-            if(check[cur]) continue;
-            // 방문 처리
-            check[cur] = true;
+        while (!pq.isEmpty()) {
+            Node cur = pq.poll();
+            if (cur.cost > dist[cur.idx]) continue;
 
-            for(int i = 1; i <= vertex; i++){
-                if(check[i] == false && dist[i] > dist[cur] + arr[cur][i]){
-                    dist[i] = dist[cur] + arr[cur][i];
-                    queue.add(new Node(i, dist[i]));
+            for (Node next : graph.get(cur.idx)) {
+                int newCost = cur.cost + next.cost;
+                if (newCost < dist[next.idx]) {
+                    dist[next.idx] = newCost;
+                    pq.add(new Node(next.idx, newCost));
                 }
             }
         }
+        return dist;
     }
 }
