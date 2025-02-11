b9858316c053554873bafab846796a73d57ad314
MinSeok_Choi <120546936+m0304s@users.noreply.github.com>
2025-02-11 12:40:34 +0900

    [Gold V] Title: 토마토, Time: 724 ms, Memory: 142108 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,77 +1,80 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N,M;
-    static int [][] map;
+    private static final int RIPE = 1, UNRIPE = 0, BLANK = -1;
+    private static final int [] dx = {0,0,-1,1};
+    private static final int [] dy = {-1,1,0,0};
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    static int [] dx = {0,0,-1,1};
-    static int [] dy = {1,-1,0,0};
-    
-    static class Tomato{
-        int x;
-        int y;
-        int day;
-        public Tomato(int x,int y,int day){
+    private static class Node{
+        int x,y,day;
+        public Node(int x,int y,int day){
             this.x = x;
             this.y = y;
             this.day = day;
         }
     }
-    static Queue<Tomato> queue = new LinkedList<>();
-
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    
+    private static int M,N;
+    private static int [][] map;
+    private static Queue<Node> queue;
     public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String [] tokens  = br.readLine().split(" ");
-
+        String [] tokens = br.readLine().split(" ");
         M = Integer.parseInt(tokens[0]);
         N = Integer.parseInt(tokens[1]);
-
         map = new int[N][M];
+        queue = new LinkedList<>();
 
         for(int i=0;i<N;i++){
             tokens = br.readLine().split(" ");
             for(int j=0;j<M;j++){
                 map[i][j] = Integer.parseInt(tokens[j]);
-                if(map[i][j] == 1){
-                    queue.add(new Tomato(i, j, 0));
-                }
+                if(map[i][j] == RIPE) queue.add(new Node(i,j,0));
             }
         }
+
         bfs();
         bw.flush();
+        bw.close();
+        br.close();
     }
-    static void bfs()throws IOException{
+    private static void bfs() throws IOException{
         int day = 0;
         while(!queue.isEmpty()){
-            Tomato t = queue.poll();
-            day = t.day;
-            for(int i=0;i<4;i++){
-                int nextX = t.x+dx[i];
-                int nextY = t.y+dy[i];
-                if(nextX>=0 && nextX<N && nextY>=0 && nextY<M){
-                    if(map[nextX][nextY]==0){
-                        map[nextX][nextY]=1;
-                        queue.add(new Tomato(nextX, nextY, day+1));
-                    }
+            Node curNode = queue.poll();
+            day = curNode.day;
+
+            for(int d=0;d<4;d++){
+                int nx = curNode.x + dx[d];
+                int ny = curNode.y + dy[d];
+
+                if(!inRange(nx, ny)) continue;
+                if(map[nx][ny] == UNRIPE){
+                    queue.add(new Node(nx,ny,day+1));
+                    map[nx][ny] = RIPE;
                 }
             }
         }
-        if(checkStatus()){
+
+        if(!checkStatus()){
             bw.write(day+"\n");
         }else{
-            bw.write(-1+"\n");
+            bw.write("-1\n");
         }
     }
-    static boolean checkStatus() {
-        for(int i=0; i<N; i++) {
-            for(int j=0; j<M; j++) {
-                if(map[i][j] == 0)
-                    return false;
-                // 덜 익은 토마토가 있다면
+
+    private static boolean checkStatus(){
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
+                if(map[i][j] == UNRIPE) return true;
             }
         }
-        return true;
+        return false;
+    }
+
+    private static boolean inRange(int x,int y){
+        return x >= 0 && x < N && y >= 0 && y < M;
     }
 }
