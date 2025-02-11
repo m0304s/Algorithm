4b8c72d499f0dd483f09a6cf5e0b7a712b1bd616
MinSeok_Choi <120546936+m0304s@users.noreply.github.com>
2025-02-11 10:28:31 +0900

    [Silver II] Title: DFS와 BFS, Time: 244 ms, Memory: 21896 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,62 +1,76 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    public static void main(String[] args) throws IOException{
-        String [] tokens = br.readLine().split(" ");
-        int N = Integer.parseInt(tokens[0]);    //정점의 개수
-        int M = Integer.parseInt(tokens[1]);    //간선의 개수
-        int V = Integer.parseInt(tokens[2]);    //탐색을 시작할 정점의 번호
+class Main {
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+	private static ArrayList<ArrayList<Integer>> graph;
+	private static ArrayList<Integer> dist;
+	private static int N,M,V;
+	public static void main(String [] args) throws IOException{
+		String [] tokens = br.readLine().split(" ");
+		graph = new ArrayList<>();
+		dist = new ArrayList<>();
+		N = Integer.parseInt(tokens[0]);
+		M = Integer.parseInt(tokens[1]);
+		V = Integer.parseInt(tokens[2]);
+		
+		for(int i=0;i<=N;i++) {
+			graph.add(new ArrayList<>());
+		}
+		for(int i=0;i<M;i++) {
+			tokens = br.readLine().split(" ");
+			int a = Integer.parseInt(tokens[0]);
+			int b = Integer.parseInt(tokens[1]);
+			
+			graph.get(a).add(b);
+			graph.get(b).add(a);
 
-        int [][] wires = new int[N+1][N+1];
-        boolean [] visited = new boolean[N+1];
-        int [][] wires2 = new int[N+1][N+1];
-        boolean [] visited2 = new boolean[N+1];
-        for(int i=0;i<M;i++){
-            tokens = br.readLine().split(" ");
-            int v1 = Integer.parseInt(tokens[0]);
-            int v2 = Integer.parseInt(tokens[1]);
-
-            wires[v1][v2] = 1;
-            wires[v2][v1] = 1;
-            wires2[v1][v2] = 1;
-            wires2[v2][v1] = 1;
-        }
-        dfs(V, wires, visited);
-        bw.write("\n");
-        bfs(V, wires2, visited2);
-        bw.flush();
-    }
-    public static void bfs(int v, int[][] wires, boolean[] visited) throws IOException{
-        Queue<Integer> q = new LinkedList<>();
-
-        q.add(v);
-        visited[v] = true;
-
-        while(!q.isEmpty()){
-            v = q.poll();
-            bw.write(v+" ");
-            for(int i=0;i<wires.length;i++){
-                if(wires[v][i] == 1 && !visited[i]){
-                    q.add(i);
-                    visited[i] = true;
-                }
-            }
-        }bw.write("\n");
-    }
-
-    public static void dfs(int v, int[][] wires, boolean[] visited) throws IOException{
-        if(visited[v]){
-            return;
-        }
-        bw.write(v+" ");
-        visited[v] = true;
-        for(int i=0;i<visited.length;i++){
-            if(wires[v][i]==1 && !visited[i]){
-                dfs(i, wires, visited);
-            }
-        }
-    }
+		}
+		
+		for(int i=0;i<graph.size();i++) {
+			Collections.sort(graph.get(i));
+		}
+		
+		dfs(V, new boolean[N+1]);
+		for(int i : dist) {
+			bw.write(i + " ");
+		}bw.write("\n");
+		
+		dist.clear();
+		bfs(V,new boolean[N+1]);
+		for(int i : dist) {
+			bw.write(i + " ");
+		}bw.write("\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+	private static void dfs(int node, boolean [] visited) {
+		if(visited[node]) return;
+		
+		visited[node] = true;
+		dist.add(node);
+		for(int next : graph.get(node)) {
+			dfs(next,visited);
+		}
+	}
+	
+	private static void bfs(int start, boolean [] visited) {
+		Queue<Integer> queue = new LinkedList<>();
+		visited[start] = true;
+		queue.add(start);
+		
+		while(!queue.isEmpty()) {
+			int curNode = queue.poll();
+			dist.add(curNode);
+			for(int nextNode : graph.get(curNode)) {
+				if(visited[nextNode]) continue;
+				
+				visited[nextNode] = true;
+				queue.add(nextNode);
+			}
+		}
+	}
 }
