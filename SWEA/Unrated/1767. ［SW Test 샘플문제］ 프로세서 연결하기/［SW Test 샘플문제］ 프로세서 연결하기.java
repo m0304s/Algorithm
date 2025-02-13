0a4698d7b8b619db75f86bb5fb4ef25506cbad87
MinSeok_Choi <120546936+m0304s@users.noreply.github.com>
2025-02-13 15:58:06 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 501 ms, Memory: 122,272 KB -BaekjoonHub

--- SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
+	private static final int BLANK = 0,CORE = 1, WIRE = 2;
 	private static final int [] dx = {0,0,-1,1};
 	private static final int [] dy = {-1,1,0,0};
 	
@@ -9,18 +10,8 @@ public class Solution {
 	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	
 	private static ArrayList<Core> cores;
+	private static int minLength,maxCore,N;
 	private static int [][] map;
-	private static int N;
-	private static int maxCores,minLength;
-	private static class Node{
-		int x,y;
-		
-		public Node(int x,int y) {
-			this.x = x;
-			this.y =y;
-		}
-	}
-	
 	private static class Core{
 		int x,y;
 		
@@ -31,12 +22,20 @@ public class Solution {
 		
 		public String toString() {
 			return "X : " + this.x + " Y : " + this.y;
- 		}
+		}
+	}
+	
+	private static class Node{
+		int x,y;
+		
+		public Node(int x,int y) {
+			this.x = x;
+			this.y = y;
+		}
 	}
 	
 	public static void main(String [] args) throws IOException{
 		int T = Integer.parseInt(br.readLine());
-		
 		for(int t=1;t<=T;t++) {
 			solution();
 			bw.write("#"+t+" "+minLength+"\n");
@@ -48,77 +47,72 @@ public class Solution {
 	
 	private static void solution() throws IOException{
 		N = Integer.parseInt(br.readLine());
-		maxCores = Integer.MIN_VALUE;
-		minLength = Integer.MAX_VALUE;
-		
 		map = new int[N][N];
 		cores = new ArrayList<>();
+		minLength = Integer.MAX_VALUE;
+		maxCore = Integer.MIN_VALUE;
 		for(int i=0;i<N;i++) {
 			String [] tokens = br.readLine().split(" ");
 			for(int j=0;j<N;j++) {
 				map[i][j] = Integer.parseInt(tokens[j]);
-				if(map[i][j] == 1 && i != 0 && j != 0 && i != N-1 && j != N-1) {
+				if(map[i][j] == CORE && i != 0 && j != 0 && i != N-1 && j != N-1) {
 					cores.add(new Core(i,j));
 				}
 			}
 		}
-		
 		backtracking(0,0,0);
 	}
 	
-	private static void backtracking(int depth, int coreCnt, int totalLength) {
+	private static void backtracking(int depth, int coreCnt,int totalLength) {
 		if(depth == cores.size()) {
-			//전체 탐색할 수 있는 코어 탐색한 경우..
-			if(maxCores < coreCnt) {
-				maxCores = coreCnt;
+			if(maxCore < coreCnt) {
 				minLength = totalLength;
-			}else if(maxCores == coreCnt) {
+				maxCore = coreCnt;
+			}else if(maxCore == coreCnt) {
 				minLength = Math.min(minLength, totalLength);
 			}
 			return;
 		}
 		
-		Core currentCore = cores.get(depth);
-		
 		backtracking(depth+1,coreCnt,totalLength);
 		
+		Core core = cores.get(depth);
 		for(int d=0;d<4;d++) {
-			int wireLength = createLine(currentCore,d,2);
-			
-			if(wireLength != -1) {
-				backtracking(depth+1,coreCnt+1,totalLength + wireLength);
-				createLine(currentCore,d,0);
+			int length = createLine(core,d,WIRE);
+			if(length != -1) {
+				backtracking(depth+1,coreCnt+1,totalLength+length);
+				createLine(core,d,BLANK);
 			}
 		}
 	}
 	
-	private static int createLine(Core core, int dir, int value) {
-		int wireLength = 0;
+	private static int createLine(Core core, int d, int value) {
+		int length = 0;
+		List<Node> path = new ArrayList<>();
 		
 		int x = core.x;
 		int y = core.y;
 		
-		List<Node> path = new ArrayList<>();
-		
-		while(inRange(x + dx[dir],y+dy[dir])) {
-			x += dx[dir];
-			y += dy[dir];
+		while(inRange(x + dx[d], y + dy[d])) {
+			x += dx[d];
+			y += dy[d];
 			
-			if(value == 2 && map[x][y] != 0) {
+			if(value == WIRE && map[x][y] != BLANK) {
 				for(Node node : path) {
-					map[node.x][node.y] = 0; 
+					map[node.x][node.y] = BLANK; 
 				}
 				return -1;
 			}
 			
 			map[x][y] = value;
 			path.add(new Node(x,y));
-			wireLength++;
+			length++;
 		}
-		return wireLength;
+		return length;
 	}
 	
 	private static boolean inRange(int x,int y) {
 		return x >= 0 && x < N && y >= 0 && y < N;
 	}
 }
+
