817236863be6c71ddae013afe5cbe57ad4724df0
MinSeok_Choi <120546936+m0304s@users.noreply.github.com>
2025-02-16 22:30:40 +0900

    [D2] Title: 나무 높이, Time: 90 ms, Memory: 26,240 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -1,60 +1,60 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.*;
+import java.io.*;
 
 public class Solution {
-    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         int T = Integer.parseInt(br.readLine());
         for(int t=1;t<=T;t++){
             int answer = solution();
             bw.write("#"+t+" "+answer+"\n");
         }
+
         bw.flush();
         bw.close();
         br.close();
     }
 
     private static int solution() throws IOException{
-        int size = Integer.parseInt(br.readLine());
-        int [] treeHeight = new int[size];
-
+        int N = Integer.parseInt(br.readLine());
+        int [] arr = new int[N];
         String [] tokens = br.readLine().split(" ");
 
-        int maxHeight = Integer.MIN_VALUE;
-        for(int i=0;i<tokens.length;i++){
-            treeHeight[i] = Integer.parseInt(tokens[i]);
-            maxHeight = Math.max(maxHeight,treeHeight[i]);
+        int maxHeight = 0;
+        for(int i=0;i<N;i++){
+            arr[i] = Integer.parseInt(tokens[i]);
+            maxHeight = Math.max(maxHeight, arr[i]);
         }
 
         int odd = 0;
         int even = 0;
 
-        for(int i=0;i<tokens.length;i++){
-            int diff = maxHeight - treeHeight[i];
-
+        for(int i=0;i<N;i++){
+            int diff = maxHeight - arr[i];
+            
             if(diff == 0) continue;
 
-            even += diff / 2;
             odd += diff % 2;
+            even += diff / 2;
         }
 
         if(even > odd){
-            while (Math.abs(even - odd) > 1){
+            while(Math.abs(even - odd) > 1){
                 even--;
-                odd+=2;
+                odd += 2;
             }
         }
 
         int result = 0;
-        if(odd > even) result = odd * 2 -1;
-        else if(even > odd) result = even * 2;
-        else result = odd + even;
-
+        if(odd > even){
+            result = odd * 2 -1;
+        }else if(even > odd){
+            result = even * 2;
+        }else{
+            result = odd + even;
+        }
         return result;
     }
 }
