af171efc1d45cd64b63793dfff2be2f53bb6cf2a
MinSeok_Choi <120546936+m0304s@users.noreply.github.com>
2025-02-19 15:38:25 +0900

    [D3] Title: 규영이와 인영이의 카드게임, Time: 2,696 ms, Memory: 26,880 KB -BaekjoonHub

--- SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
+++ SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
@@ -3,79 +3,90 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Solution {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-    static int [] guyoung;
-    static int [] minyoung;
-    static int [] minyoungPerm = new int[9];
-    static boolean[] visited;
-
-    static int guyoungCnt;
-    static int minyoungCnt;
-    public static void main(String[] args) throws IOException {
-        int T = Integer.parseInt(br.readLine());
-
-        for(int t=1;t<=T;t++){
-            guyoung = new int[9];
-            minyoung = new int[9];
-            visited = new boolean[9];
-
-            guyoungCnt = 0;
-            minyoungCnt = 0;
-
-            String [] tokens = br.readLine().split(" ");
-            for(int i=0;i<tokens.length;i++){
-                guyoung[i] = Integer.parseInt(tokens[i]);
-            }
-            Set<Integer> guyoungSet = new HashSet<>();
-            for (int i : guyoung) {
-                guyoungSet.add(i);
-            }
-            int index = 0;
-            for(int i=1;i<=18;i++){
-                if(!guyoungSet.contains(i)) minyoung[index++] = i;
-            }
-
-            calcScore(0);
-            bw.write("#" + t + " " + guyoungCnt + " " + minyoungCnt + "\n");
-        }
-        bw.flush();
-        bw.close();
-        br.close();
-    }
-    static void calcScore(int depth){
-        if(depth == 9){
-            int guyoungScore = 0;
-            int minyoungScore = 0;
-
-            for(int i=0;i<9;i++){
-                if(guyoung[i] > minyoungPerm[i]){
-                    guyoungScore+=(guyoung[i] + minyoungPerm[i]);
-                }else if(guyoung[i] < minyoungPerm[i]){
-                    minyoungScore+=(guyoung[i] + minyoungPerm[i]);
-                }
-            }
-
-            if(guyoungScore > minyoungScore){
-                guyoungCnt++;
-            }else if(guyoungScore < minyoungScore){
-                minyoungCnt++;
-            }
-            return;
-        }
-
-        for(int i=0;i<9;i++){
-            if(!visited[i]){
-                visited[i] = true;
-                minyoungPerm[depth] = minyoung[i];
-                calcScore(depth+1);
-                visited[i] = false;
-            }
-        }
-    }
+	/**
+	 * 규영이가 내는 카드 순서는 고정되어 있음
+	 * 규영이가 가진 카드 -> 인영이는 가질 수 없는 카드
+	 * 
+	 * 인영이가 가진 카드 Set 구함
+	 * 인영이가 낼 수 있는 카드 조합 계산 => Permutation 연산으로 완전 탐색
+	 * 만약 인영이가 내는 순서를 다 결정한 경우 => 인영이와 규영이가 1장씩 카드를 내서 규영이가 이기는지, 인영이가 이기는지 체크
+	 * 
+	 * 만약 결정하지 않은 경우 => 현재 단계에서 낼 카드를 선택한 뒤 재귀호출을 통해 다음 제출할 카드 선택
+	 * @param args
+	 */
+	
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+	private static int guyongWin, inyoungWin;
+	private static int [] guyoungCards, inyoungCards;
+	public static void main(String[] args) throws IOException{
+		int T = Integer.parseInt(br.readLine());
+		for(int t=1;t<=T;t++) {
+			solution();
+			bw.write("#"+t+" "+guyongWin +" "+inyoungWin+"\n");
+		}
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+	
+	private static void solution() throws IOException{
+		String [] tokens = br.readLine().split(" ");
+		HashSet<Integer> set = new HashSet<>();
+		guyongWin = inyoungWin = 0;
+		guyoungCards = new int[9];
+		inyoungCards = new int[9];
+		
+		for(int i=0;i<tokens.length;i++) {
+			guyoungCards[i] = Integer.parseInt(tokens[i]);
+			set.add(guyoungCards[i]);
+		}
+		
+		int size = 0;
+		for(int i=1;i<=18;i++) {
+			int num = i;
+			
+			if(set.contains(num)) continue;
+			
+			inyoungCards[size++] = i;
+		}
+		
+		permutation(0,new int[9],new boolean[9]);
+	}
+	
+	private static void permutation(int depth, int[] dist, boolean [] visited) {
+		if(depth == 9) {
+			//카드를 서로 내면서 점수 계산
+			
+			int inyoung = 0;
+			int guyoung = 0;
+			
+			for(int i=0;i<9;i++) {
+				if(dist[i] > guyoungCards[i]) {
+					inyoung+=(dist[i] + guyoungCards[i]);
+				}else if(dist[i] < guyoungCards[i]) {
+					guyoung+=(dist[i] + guyoungCards[i]);
+				}
+			}
+			
+			if(inyoung > guyoung) {
+				inyoungWin++;
+			}else if(inyoung < guyoung) {
+				guyongWin++;
+			}
+		}
+		
+		for(int i=0;i<9;i++) {
+			if(visited[i]) continue;
+			
+			visited[i] = true;
+			dist[depth] = inyoungCards[i];
+			permutation(depth+1, dist, visited);
+			visited[i] = false;
+		}
+	}
 }
