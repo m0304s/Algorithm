b17c2d9a1b9264bfd37f8f6e4f70e75f785fa221
MinSeok_Choi <120546936+m0304s@users.noreply.github.com>
2025-02-20 14:07:40 +0900

    [Bronze II] Title: 블랙잭, Time: 120 ms, Memory: 15848 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,30 +1,46 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner kb = new Scanner(System.in);
-        int N = kb.nextInt();   //카드의 개수
-        int M = kb.nextInt();   //넘지 않아야 하는 수
-        int [] array = new int[N];
-        for(int i=0;i<N;i++){
-            array[i]=kb.nextInt();
-        }
-        int max = 0;    //최대한 M에 가까운 합
-        for(int i=0;i<array.length;i++){
-            for(int j=i+1;j<array.length;j++){
-                for(int k=j+1;k<array.length;k++){
-                    if(array[i]+array[j]+array[k]>M){
-                        continue;
-                    }else if(array[i]+array[j]+array[k]<=M){
-                        if(max<array[i]+array[j]+array[k]){
-                            max=array[i]+array[j]+array[k];
-                        }
-                    }
-                }
-            }
-        }
-        System.out.println(max);
-
-        kb.close();
-    }
+	/**
+	 * 문제 설계
+	 * 뽑는 카드 조합 계산
+	 * 재귀함수의 매개변수에 뽑은 카드 숫자를 포함
+	 * 
+	 */
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	private static int N,M, maxSum = Integer.MIN_VALUE;
+	private static int [] cards;
+	public static void main(String[] args) throws IOException{
+		String [] tokens = br.readLine().split(" ");
+		N = Integer.parseInt(tokens[0]);
+		M = Integer.parseInt(tokens[1]);
+		cards = new int[N];
+		tokens = br.readLine().split(" ");
+		for(int i=0;i<N;i++) {
+			cards[i] = Integer.parseInt(tokens[i]);
+		}
+		backtracking(0, 0, 0);
+		bw.write(maxSum+"\n");
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+	private static void backtracking(int depth, int totalSum, int selectedCnt) {
+		if(totalSum > M) return;
+		
+		if(selectedCnt == 3) {
+			maxSum = Math.max(maxSum, totalSum);
+			return;
+		}
+		
+		if(depth >= N) return;
+		
+		backtracking(depth+1, totalSum+cards[depth], selectedCnt+1);
+		backtracking(depth+1, totalSum, selectedCnt);
+	}
 }
