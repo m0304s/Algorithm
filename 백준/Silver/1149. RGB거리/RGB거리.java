8cc64e297cd4aece27a3a55b4a193a99424795ae
MinSeok_Choi <120546936+m0304s@users.noreply.github.com>
2025-04-04 12:40:21 +0900

    [Silver I] Title: RGB거리, Time: 132 ms, Memory: 15948 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,27 +1,42 @@
 import java.io.*;
 
 public class Main {
-    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    public static void main(String[] args) throws IOException{
-        int N = Integer.parseInt(br.readLine());
-        int [][] price = new int[N+1][3];
-        int [][] answer = new int[N+1][3];
-        price[0][0]=price[0][1]=price[0][2]=answer[0][0]=answer[0][1]=answer[0][2]=0;
-        for(int i=1;i<=N;i++){
-            String[] token = br.readLine().split(" ");
-            for(int j=0;j<token.length;j++){
-                price[i][j] = Integer.parseInt(token[j]);
-            }
-        }
-        for(int i=1;i<=N;i++){
-            answer[i][0]=Math.min(answer[i-1][1],answer[i-1][2])+price[i][0];
-            answer[i][1]=Math.min(answer[i-1][0],answer[i-1][2])+price[i][1];
-            answer[i][2]=Math.min(answer[i-1][0],answer[i-1][1])+price[i][2];
-        }
-        int sum = Math.min(Math.min(answer[N][0],answer[N][1]),answer[N][2]);
-        bw.write(Integer.toString(sum)+"\n");
-        bw.flush();
-        bw.close();
-    }
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+	static int N;
+	static int [][] color;
+	static int [][] costs;
+	
+	public static void main(String[] args) throws IOException{
+		N = Integer.parseInt(br.readLine());
+		color = new int[N][3];
+		costs = new int[N][3];
+		
+		for(int i=0;i<N;i++) {
+			String [] tokens = br.readLine().split(" ");
+			color[i][0] = Integer.parseInt(tokens[0]);
+			color[i][1] = Integer.parseInt(tokens[1]);
+			color[i][2] = Integer.parseInt(tokens[2]);
+		}
+		
+		costs[0][0] = color[0][0];
+		costs[0][1] = color[0][1];
+		costs[0][2] = color[0][2];
+		
+		costs[1][0] = Math.min(costs[0][1] + color[1][0], costs[0][2] + color[1][0]);
+		costs[1][1] = Math.min(costs[0][0] + color[1][1], costs[0][2] + color[1][1]);
+		costs[1][2] = Math.min(costs[0][0] + color[1][2], costs[0][1] + color[1][2]);
+		
+		for(int i=2;i<N;i++) {
+			costs[i][0] = Math.min(costs[i-1][1] + color[i][0], costs[i-1][2] + color[i][0]);
+			costs[i][1] = Math.min(costs[i-1][0] + color[i][1], costs[i-1][2] + color[i][1]);
+			costs[i][2] = Math.min(costs[i-1][0] + color[i][2], costs[i-1][1] + color[i][2]);
+		}
+		
+		bw.write(Math.min(Math.min(costs[N-1][0], costs[N-1][1]), costs[N-1][2])+"\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
 }
