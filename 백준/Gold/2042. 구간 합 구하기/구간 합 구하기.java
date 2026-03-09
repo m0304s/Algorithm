2f7ecffe2e4cb05c2a2e0f5643580e9d898e7c73
MinSeok_Choi <120546936+m0304s@users.noreply.github.com>
2026-03-09 14:30:18 +0900

    [Gold I] Title: 구간 합 구하기, Time: 700 ms, Memory: 121412 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -4,110 +4,92 @@ public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	
-	static int N,M,K;
 	static long [] arr;
-	static class SegmentTree{
-		long [] tree;
-		int treeSize;
-		
-		public SegmentTree(int arrSize) {
-			int h = (int) Math.ceil(Math.log(arrSize) / Math.log(2));
-			
-			this.treeSize = 1 << (h + 1);
-			this.tree = new long[treeSize];
-		}
-		
-		/**
-		 * @param arr : 원소 배열
-		 * @param node : 현재 노드
-		 * @param start : 탐색 구간 배열 시작
-		 * @param end : 탐색 구간 배열 끝
-		 * @return
-		 */
-		public void init(long[] arr, int node, int start, int end) {
-		    if (start == end) {
-		        tree[node] = arr[start];
-		    } else {
-		        int mid = (start + end) / 2;
-		        init(arr, node * 2, start, mid);
-		        init(arr, node * 2 + 1, mid + 1, end);
-		        tree[node] = tree[node * 2] + tree[node * 2 + 1];
-		    }
-		}
-
-		
-		/**
-		 * 
-		 * @param node 현재 노드
-		 * @param start 배열의 시작
-		 * @param end 배열의 끝
-		 * @param idx 변경된 데이터의 idx
-		 * @param diff 변경된 데이터 변화량
-		 */
-		public void update(int node, int start, int end, int idx, long diff) {
-			if(idx < start || idx > end) return;
-			
-			tree[node] += diff;
-			
-			//리프노드가 아니라면 아래 자식들도 변화
-			if(start != end) {
-				int mid = (start + end) /2;
-				update(node*2,start,mid,idx,diff);
-				update(node*2+1,mid+1,end,idx,diff);
-			}
-		}
-		
-		public long sum(int node, int start, int end, int left, int right) {
-			//범위를 전부 벗어나게 되는 경우
-			if(left > end || right < start) {
-				return 0;
-			}
-			
-			//범위 내 완전히 포함되는 경우
-			if(left <= start && end <= right) {
-				return tree[node];
-			}
-			
-			int mid = (start + end)/2;
-			return sum(node*2,start,mid,left,right) +
-					sum(node*2+1,mid+1,end,left,right);
-		}
-	}
+	static long [] segTree;
+	static int N,M,K;
 	
-	public static void main(String[] args) throws IOException{
+	public static void main(String [] args) throws IOException{
 		String [] tokens = br.readLine().split(" ");
-		N = Integer.parseInt(tokens[0]);	//수의 개수
-		M = Integer.parseInt(tokens[1]);	//변경이 일어나는 횟수
-		K = Integer.parseInt(tokens[2]);	//구간의 합을 구하는 횟수
 		
-		arr = new long[N];
-		for(int i=0;i<N;i++) {
+		N = Integer.parseInt(tokens[0]);
+		M = Integer.parseInt(tokens[1]);
+		K = Integer.parseInt(tokens[2]);
+		
+		arr = new long[N+1];
+		segTree = new long[4*N+1];
+		
+		for(int i=1;i<=N;i++) {
 			arr[i] = Long.parseLong(br.readLine());
 		}
 		
-		SegmentTree st = new SegmentTree(N);
-		st.init(arr, 1, 0, N-1);
+		init(1, 1, N);
+		
 		for(int i=0;i<M+K;i++) {
 			tokens = br.readLine().split(" ");
-			int type = Integer.parseInt(tokens[0]);
-			if(type == 1) {
-				int idx = Integer.parseInt(tokens[1])-1;
-				long value = Long.parseLong(tokens[2]);
-				long diff = value - arr[idx];
-				st.update(1, 0, N-1, idx, diff);
-				arr[idx] = value; // arr 배열을 갱신
+			int trigger = Integer.parseInt(tokens[0]);
+			int b = Integer.parseInt(tokens[1]);
+			long c = Long.parseLong(tokens[2]);
 
+			if(trigger == 1) {
+				long diff = c - arr[b];
+				arr[b] = c;
+				update(1, 1, N, b, diff);
 			}else {
-				int left = Integer.parseInt(tokens[1])-1;
-				int right = Integer.parseInt(tokens[2])-1;
-				
-				long result = st.sum(1, 0, N-1, left, right);
-				bw.write(result+"\n");
+				bw.write(query(1, 1, N, b, c)+"\n");
+				bw.flush();
 			}
 		}
-		bw.flush();
 		bw.close();
 		br.close();
 	}
-
+	
+	/**
+	 * node : 현재 노드의 번호
+	 * start : 담당 구간의 시작
+	 * end : 담당 구간의 종료
+	 */
+	private static long init(int node, int start, int end) {
+		if(start == end) return segTree[node] = arr[start];
+		
+		int mid = (start + end) / 2;
+			
+		return segTree[node] = init(node * 2, start, mid) + init(node*2 + 1, mid+1, end);
+	}
+	
+	/**
+	 * node : 현재 노드의 번호
+	 * start : 담당 구간의 시작
+	 * end : 담당 구간의 종료
+	 * idx : 원본 배열의 인덱스
+	 * diff : 변화량
+	 */
+	private static void update(int node, int start, int end, int idx, long diff) {
+		//조회 구간이 전혀 영향이 없을 때
+		if(idx < start || idx > end) return;
+		
+		//구간 내에 포함될 경우 값을 업데이트
+		segTree[node] += diff;
+		
+		if(start != end) {
+			int mid = (start + end) / 2;
+			update(node * 2, start, mid, idx, diff);
+			update(node * 2 + 1, mid+1, end, idx, diff);	
+		}
+	}
+	
+	/**
+	 * node : 현재 노드의 번호
+	 * start : 담당 구간의 시작
+	 * end : 담당 구간의 종료
+	 * left : 조회 구간의 시작
+	 * right : 조회 구간의 종료
+	 */
+	private static long query(int node, int start, int end, int left, long right) {
+		if(right < start || end < left) return 0;
+		
+		if(left <= start && end <= right) return segTree[node];
+		
+		int mid = (start + end) / 2;
+		return query(node*2, start, mid, left, right) + query(node*2 + 1, mid+1, end, left, right);
+	}
 }
