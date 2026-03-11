a25432ab8dc64d4e614e6d6d32ff5dbf081e16f5
MinSeok_Choi <120546936+m0304s@users.noreply.github.com>
2026-03-11 16:35:06 +0900

    [Gold I] Title: 구간 곱 구하기, Time: 516 ms, Memory: 109452 KB -BaekjoonHub

--- 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.java
+++ 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.java
@@ -4,20 +4,19 @@ public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	
-	static long [] tree;
-	static long [] arr;
 	static int N,M,K;
+	static int [] arr;
+	static long [] segTree;
 	static int MOD = 1000000007;
 	
-	public static void main(String [] args) throws IOException{
+	public static void main(String[] args) throws IOException{
 		String [] tokens = br.readLine().split(" ");
+		N = Integer.parseInt(tokens[0]);
+		M = Integer.parseInt(tokens[1]);
+		K = Integer.parseInt(tokens[2]);
 		
-		N = Integer.parseInt(tokens[0]);	//수의 개수
-		M = Integer.parseInt(tokens[1]);	//수의 변경이 일어나는 횟수
-		K = Integer.parseInt(tokens[2]);	//구간의 곱을 구하는 횟수
-		
-		tree = new long[4*N];
-		arr = new long[N+1];	//배열 인덱스는 1부터 시작
+		arr = new int[N+1];
+		segTree = new long[4*N+1];
 		
 		for(int i=1;i<=N;i++) {
 			arr[i] = Integer.parseInt(br.readLine());
@@ -29,14 +28,16 @@ public class Main {
 			tokens = br.readLine().split(" ");
 			int a = Integer.parseInt(tokens[0]);
 			int b = Integer.parseInt(tokens[1]);
-			long c = Long.parseLong(tokens[2]);
+			int c = Integer.parseInt(tokens[2]);
 			
 			if(a == 1) {
-				//b번쨰 수를 c로 변경
-				update(1, 1, N, b, c);
+				//b번째 수를 c로 바꿈
 				arr[b] = c;
+				update(1, 1, N, b, c);
 			}else {
-				bw.write(query(1, 1, N, b, (int)c) +"\n");
+				//b부터 c까지의 곱을 출력
+				long answer = query(1, 1, N, b, c);
+				bw.write(answer+"\n");
 			}
 		}
 		
@@ -45,42 +46,57 @@ public class Main {
 		br.close();
 	}
 	
-	static long query(int node, int start, int end, int left, int right) {
-		if(left > end || right < start) {
-			return 1;
-		}
+	/**
+	 * @param node : 트리의 번호
+	 * @param start : 현재 노드가 관리하는 구간 시작점
+	 * @param end : 현재 노드가 관리하는 구간 종료점
+	 * @param left : 조회 시작 구간
+	 * @param right : 조회 종료 구간
+	 * @return
+	 */
+	private static long query(int node, int start, int end, int left, int right) {
+		//완전히 구간을 벗어나는 경우
+		if(end < left || right < start) return 1;
 		
-		if(left <= start && end <= right) {
-			return tree[node];
-		}
+		//구간에 완전히 포함되는 경우
+		if(left <= start && end <= right) return segTree[node];
 		
 		int mid = (start + end) / 2;
-		
-		return query(node * 2, start, mid, left, right) * query(node * 2 + 1, mid + 1, end, left, right) % MOD;
+		return (query(node*2, start, mid, left, right) * query(node*2+1, mid+1, end, left, right)) % MOD;
 	}
 	
-	static long init(int node, int start, int end) {
+	/**
+	 * @param node : 트리의 번호
+	 * @param start : 현재 노드가 관리하는 구간 시작점
+	 * @param end : 현재 노드가 관리하는 구간 종료점
+	 * @return
+	 */
+	private static long init(int node, int start, int end) {
 		if(start == end) {
-			return tree[node] = arr[start];
+			return segTree[node] = arr[start];
 		}
 		
 		int mid = (start + end) / 2;
-		
-		return tree[node] = init(node * 2, start, mid) * init(node * 2 + 1, mid + 1, end) % MOD;
+		return segTree[node] = (init(node*2, start, mid) * init(node*2+1, mid+1, end)) % MOD;
 	}
 	
-	static long update(int node, int start, int end, int index, long val) {
-		if(index < start || index > end) {
-			return tree[node];
-		}
+	/**
+	 * @param node : 트리의 번호
+	 * @param start : 현재 노드가 관리하는 구간 시작점
+	 * @param end : 현재 노드가 관리하는 구간 종료점
+	 * @param idx : 값이 변하는 arr 인덱스
+	 * @param diff : 값의 변화량
+	 * @return
+	 */
+	private static long update(int node, int start, int end, int idx, int diff) {
+		//만약 구간을 완전히 벗어나면
+		if(idx < start || idx > end) return segTree[node];
 		
 		if(start == end) {
-			return tree[node] = val;
+			return segTree[node] = diff;
 		}
 		
 		int mid = (start + end) / 2;
-		return tree[node] = update(node * 2, start, mid , index, val) * update(node * 2 + 1, mid + 1, end, index, val) % MOD;
+		return segTree[node] = (update(node*2, start, mid, idx, diff) * update(node*2+1, mid+1, end, idx, diff)) % MOD;
 	}
-	
-	
 }
